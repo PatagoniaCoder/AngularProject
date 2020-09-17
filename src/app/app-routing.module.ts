e502393af08cee8e39dd7d6bfa7ec6d36399aab6
Alejandro <menendezanm@gmail.com>
2020-09-17 08:55:03 -0300

    Adding Entries component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import {} from "./customer/customer.module";
+import {} from "./entries/entries.module";
 export const routes: Routes = [
   {
     path: "",
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: "customer",
     loadChildren: "./customer/customer.module#CustomerModule",
+  },
+  {
+    path: "entries",
+    loadChildren: "./entries/entries.module#EntriesModule",
   },
   { path: "**", redirectTo: "/" },
 ];
